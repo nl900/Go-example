8a068495fb89fd0cbd00688e40410edf38168de0
nl900 <nancy.lee900@gmail.com>
2021-12-23 13:13:31 +0100

    randomise greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,28 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+// Capitalised, accessible to outside its package
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() {
+	formats := []string{
+		"hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
